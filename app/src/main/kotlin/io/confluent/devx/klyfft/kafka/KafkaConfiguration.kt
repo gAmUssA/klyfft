bb456945765e944988066772cf5ccd23415e68fa
Viktor Gamov <viktor@confluent.io>
2019-10-22 15:36:46 -0400

    Added ccloud config

--- app/src/main/kotlin/io/confluent/devx/klyfft/kafka/KafkaConfiguration.kt
+++ app/src/main/kotlin/io/confluent/devx/klyfft/kafka/KafkaConfiguration.kt
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.annotation.EnableKafka
 
-
 @Configuration
 @EnableKafka
 class KafkaConfiguration {
