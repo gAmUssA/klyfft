76ee6043a444facf227163f86f9b225328d765ff
fedor <fedor.korotkov@gmai.com>
2019-10-22 12:10:30 -0400

    deployment module

---
+++ deployment/src/main/kotlin/io/confluent/devx/klyfft/KlyfftDeployment.kt
@@ -0,0 +1,12 @@
+package io.confluent.devx.klyfft
+
+import com.fkorotkov.kubernetes.client.DefaultKafkaClient
+import io.fabric8.kubernetes.client.Config
+import io.fabric8.kubernetes.client.DefaultKubernetesClient
+
+fun main() {
+  val config = Config.autoConfigure(null)
+  val client = DefaultKubernetesClient(config).inNamespace("default")
+
+  val kafkaClient = DefaultKafkaClient().inNamespace("operator")\
+}
