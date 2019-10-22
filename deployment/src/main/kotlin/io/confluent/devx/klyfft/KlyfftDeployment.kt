802dab487b5d44c0c3ca4a55750bf1c6e359605a
fedor <fedor.korotkov@gmai.com>
2019-10-22 15:17:28 -0400

    Replicate Klyfft Deployment via Kotlin DSL

--- deployment/src/main/kotlin/io/confluent/devx/klyfft/KlyfftDeployment.kt
+++ deployment/src/main/kotlin/io/confluent/devx/klyfft/KlyfftDeployment.kt
@@ -1,12 +1,43 @@
 package io.confluent.devx.klyfft
 
 import com.fkorotkov.kubernetes.client.DefaultKafkaClient
+import com.fkorotkov.kubernetes.newContainer
+import com.fkorotkov.kubernetes.newEnvVar
+import io.confluent.devx.klyfft.base.BaseDeployment
 import io.fabric8.kubernetes.client.Config
 import io.fabric8.kubernetes.client.DefaultKubernetesClient
 
 fun main() {
   val config = Config.autoConfigure(null)
   val client = DefaultKubernetesClient(config).inNamespace("default")
-
   val kafkaClient = DefaultKafkaClient().inNamespace("operator")
+
+  val klyfftDeployment = BaseDeployment("klyfft").apply {
+    spec.template.spec.containers = listOf(
+        newContainer {
+          name = serviceName
+          image = "gcr.io/cloud-private-dev/gamussa/klyfft:latest"
+          env = listOf(
+              newEnvVar {
+                name = "JAVA_TOOL_OPTIONS"
+                value = "-DLOGLEVEL=INFO"
+              },
+              newEnvVar {
+                name = "SASL_JAAS_CONFIG"
+                value = "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"test\" password=\"test123\";"
+              },
+              newEnvVar {
+                name = "SASL_MECHANISM"
+                value = "PLAIN\"test\" password=\"test123\";"
+              },
+              newEnvVar {
+                name = "SECURITY_PROTOCOL"
+                value = "SASL_PLAINTEXT"
+              }
+          )
+        }
+    )
+  }
+
+  client.apps().deployments().createOrReplace(klyfftDeployment)
 }
---
+++ deployment/src/main/kotlin/io/confluent/devx/klyfft/base/BaseDeployment.kt
@@ -0,0 +1,53 @@
+package io.confluent.devx.klyfft.base
+
+import com.fkorotkov.kubernetes.*
+import com.fkorotkov.kubernetes.apps.metadata
+import com.fkorotkov.kubernetes.apps.selector
+import com.fkorotkov.kubernetes.apps.spec
+import com.fkorotkov.kubernetes.apps.template
+import io.fabric8.kubernetes.api.model.apps.Deployment
+
+class BaseDeployment(
+    val serviceName: String,
+    val defaultReplicas: Int = 1
+) : Deployment() {
+  init {
+    val defaultLabels = mapOf("app" to serviceName)
+
+    metadata {
+      name = serviceName
+      labels = defaultLabels
+    }
+    spec {
+      replicas = defaultReplicas
+      selector {
+        matchLabels = defaultLabels
+      }
+      template {
+        metadata {
+          labels = defaultLabels
+        }
+        spec {
+          affinity {
+            podAntiAffinity {
+              requiredDuringSchedulingIgnoredDuringExecution = listOf(
+                  newPodAffinityTerm {
+                    topologyKey = "kubernetes.io/hostname"
+                    labelSelector {
+                      matchExpressions = listOf(
+                          newLabelSelectorRequirement {
+                            key = "app"
+                            operator = "In"
+                            values = listOf(serviceName)
+                          }
+                      )
+                    }
+                  }
+              )
+            }
+          }
+        }
+      }
+    }
+  }
+}
