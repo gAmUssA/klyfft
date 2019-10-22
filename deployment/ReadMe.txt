6e9dcd49b1f419a416af57fa278b1ac31443cb74
fedor <fedor.korotkov@gmai.com>
2019-10-22 16:56:21 -0400

    Application Config

--- deployment/ReadMe.txt
+++ deployment/ReadMe.txt
@@ -4,3 +4,10 @@
 ./gradlew :deployment:createSecret -PsecretName=foo -PsecretValue=BAR
 kubectl get secret foo -o yaml
 ```
+
+# Create Application Config
+
+```bash
+./gradlew :deployment:createSecret -PsecretName=application-config -PsecretValue=$PWD/app/src/main/resources/application.properties
+kubectl get secret application-config -o yaml
+```
--- deployment/src/main/kotlin/io/confluent/devx/klyfft/KlyfftDeployment.kt
+++ deployment/src/main/kotlin/io/confluent/devx/klyfft/KlyfftDeployment.kt
@@ -1,8 +1,7 @@
 package io.confluent.devx.klyfft
 
+import com.fkorotkov.kubernetes.*
 import com.fkorotkov.kubernetes.client.DefaultKafkaClient
-import com.fkorotkov.kubernetes.newContainer
-import com.fkorotkov.kubernetes.newEnvVar
 import io.confluent.devx.klyfft.base.BaseDeployment
 import io.fabric8.kubernetes.client.Config
 import io.fabric8.kubernetes.client.DefaultKubernetesClient
@@ -18,6 +17,24 @@ fun main() {
           name = serviceName
           image = "gcr.io/cloud-private-dev/gamussa/klyfft:latest"
           env = listOf(
+              newEnvVar {
+                name = "MAPBOX_ACCESS_TOKEN"
+                valueFrom {
+                  secretKeyRef {
+                    name = "application-config"
+                    key = "access.token"
+                  }
+                }
+              },
+              newEnvVar {
+                name = "BOOTSTRAP_SERVERS"
+                valueFrom {
+                  secretKeyRef {
+                    name = "application-config"
+                    key = "spring.kafka.bootstrap-servers"
+                  }
+                }
+              },
               newEnvVar {
                 name = "JAVA_TOOL_OPTIONS"
                 value = "-DLOGLEVEL=INFO"
