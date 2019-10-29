a20f77c6aaf09d8c9d104452249e12cf78ab8f86
fedor <fedor.korotkov@gmai.com>
2019-10-29 10:12:30 +0300

    undeploy task

---
+++ deployment/src/main/kotlin/io/confluent/devx/klyfft/KlyfftDeleteDeployment.kt
@@ -0,0 +1,24 @@
+package io.confluent.devx.klyfft
+
+import com.fkorotkov.kubernetes.client.DefaultKafkaClient
+import io.fabric8.kubernetes.client.Config
+import io.fabric8.kubernetes.client.DefaultKubernetesClient
+import kotlin.system.exitProcess
+
+fun main() {
+  val config = Config.autoConfigure(null)
+  val client = DefaultKubernetesClient(config).inNamespace("default")
+
+  val klyfftDeployment = client.apps().deployments().list().items.find { it.metadata.name == "klyfft" }
+
+  if (klyfftDeployment == null) {
+    println("No klyfft found!")
+    exitProcess(1)
+  }
+
+  val secretNames = klyfftDeployment.spec.template.spec.volumes.mapNotNull { it.secret }.map { it.secretName }.toSet()
+  val secretsToDelete = client.secrets().list().items.filter { secretNames.contains(it.metadata.name) }
+
+  client.apps().deployments().delete(klyfftDeployment)
+  client.secrets().delete(secretsToDelete)
+}
