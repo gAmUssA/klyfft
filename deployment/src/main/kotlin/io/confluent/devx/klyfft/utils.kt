98a818a5c7978795c8106f9ce693c80be0fb3120
Viktor Gamov <viktor@confluent.io>
2019-10-29 09:49:51 +0300

    Refactor

--- deployment/src/main/kotlin/io/confluent/devx/klyfft/utils.kt
+++ deployment/src/main/kotlin/io/confluent/devx/klyfft/utils.kt
@@ -2,6 +2,5 @@ package io.confluent.devx.klyfft
 
 import java.util.*
 
-fun String.toBase64(): String {
-  return Base64.getEncoder().encodeToString(this.toByteArray()).toString()
-}
+fun String.toBase64() =
+        Base64.getEncoder().encodeToString(this.toByteArray()).toString()
