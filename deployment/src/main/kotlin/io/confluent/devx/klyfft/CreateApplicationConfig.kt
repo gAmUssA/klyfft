b9d3e721eb94bf7f5b83c63a964c7343192a91d1
fedor <fedor.korotkov@gmai.com>
2019-10-29 10:49:35 +0300

    createApplicationConfig task

---
+++ deployment/src/main/kotlin/io/confluent/devx/klyfft/CreateApplicationConfig.kt
@@ -0,0 +1,83 @@
+package io.confluent.devx.klyfft
+
+import com.fkorotkov.kubernetes.*
+import com.fkorotkov.kubernetes.client.DefaultKafkaClient
+import io.fabric8.kubernetes.client.Config
+import io.fabric8.kubernetes.client.DefaultKubernetesClient
+import java.io.File
+import java.io.FileInputStream
+import java.io.StringReader
+import java.io.StringWriter
+import java.lang.IllegalStateException
+import java.util.*
+import kotlin.system.exitProcess
+
+fun main(args: Array<String>) {
+  val config = Config.autoConfigure(null)
+  val client = DefaultKubernetesClient(config).inNamespace("default")
+  val kafkaClient = DefaultKafkaClient(config).inNamespace("operator")
+
+  if (args.size != 1) {
+    System.err.println("Expected one argument!")
+    exitProcess(1)
+  }
+
+  val (configPath) = args
+
+  val kafkaCluster = kafkaClient.kafkaClusters().list().items.firstOrNull()
+      ?: throw IllegalStateException("Can't find Kafka cluster!")
+
+  val propertiesFileContent = createProperties(
+      configPath,
+      extractSpringProperties(kafkaCluster.status.internalClient)
+  )
+
+  val secretData = mapOf(
+      "application.properties" to propertiesFileContent.toBase64()
+  )
+
+  client.secrets().createOrReplace(
+      newSecret {
+        type = "Opaque"
+        metadata {
+          name = "application-config"
+        }
+        data = secretData
+      }
+  )
+}
+
+private fun createProperties(secretValue: String, propertiesToMerge: Map<String, String>): String {
+  val propertiesFile = File(secretValue)
+
+  if (!propertiesFile.exists()) {
+    System.err.println("No properties file provided!")
+    exitProcess(1)
+  }
+
+  val properties = Properties().apply {
+    load(FileInputStream(propertiesFile))
+  }
+
+  propertiesToMerge.forEach { name, value ->
+    properties.setProperty(name, value)
+  }
+
+  val propertiesFileContent = StringWriter().also {
+    properties.store(it, "application config")
+  }.toString()
+  return propertiesFileContent
+}
+
+private fun extractSpringProperties(internalClient: String): Map<String, String> {
+  val internalClientProperties = Properties().apply {
+    load(StringReader(internalClient))
+  }
+
+  return mapOf(
+      "spring.kafka.bootstrap-servers" to internalClientProperties.getProperty("bootstrap.servers"),
+      "spring.kafka.properties.sasl.jaas.config" to internalClientProperties.getProperty("sasl.jaas.confi"),
+      "spring.kafka.properties.sasl.mechanism" to internalClientProperties.getProperty("sasl.mechanism"),
+      "spring.kafka.properties.security.protocol" to internalClientProperties.getProperty("security.protocol")
+  )
+}
